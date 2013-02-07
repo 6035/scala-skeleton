d6b8d340869b9c1dbae0cd1816b1ab83be3c9e33
Russell Cohen <rcoh@mit.edu>
2013-02-07 02:05:15 -0500

    Tweaks to the setup script.

--- setup-scala.sh
+++ setup-scala.sh
@@ -1,7 +1,3 @@
-echo "IF YOU ARE NOT USING ATHENA, CHANGE ME!"
-add scala
-# CHANGE THIS LINE IF YOU AREN'T ON ATHENA TO YOUR SCALA INSTALL
-export SCALA_HOME='/mit/scala/scala/scala-2.10.0'
-
+# This script need only be run once per repo
 # Used so test scripts can find what they need.
 git config --add alias.root '!pwd'
