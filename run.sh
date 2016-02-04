1f09f80c5c7c2f30650da0c51335e8185f5c8ab6
Jiasi Shen <jiasi@mit.edu>
2016-02-04 13:51:04 -0500

    Suppress JAVA_TOOL_OPTIONS message

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 gitroot=$(git rev-parse --show-toplevel)
+if [ -n "$JAVA_TOOL_OPTIONS" ]; then
+  export JAVA_OPTS="$JAVA_TOOL_OPTIONS"
+  unset JAVA_TOOL_OPTIONS
+fi
 scala -classpath $gitroot/build/lib/project.jar:$gitroot/lib/antlr.jar compile.Compiler "$@"
