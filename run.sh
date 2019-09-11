5bfa3c5f950dd8220d2e630526b8ac5bb72753b3
Wonsuk Choi <jchoi5me@mit.edu>
2019-09-11 18:23:54 -0400

    exports scala home in run

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 declare -r ROOT=$(git rev-parse --show-toplevel)
 declare -r JARS=$(find "$ROOT" -name '*jar' | xargs | sed 's/ /:/g')
 
+export SCALA_HOME='/usr/share/scala-2.11'
+
 scala -classpath "$JARS" compile.Compiler "$@"
