5ab3af71c306b226d286c13e39ff147f113b5f11
Wonsuk Choi <jchoi5me@mit.edu>
2019-09-12 23:02:10 -0400

    correct handling of whitespace in jar path of run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 declare -r ROOT=$(git rev-parse --show-toplevel)
-declare -r JARS=$(find "$ROOT" -name '*jar' | xargs | sed 's/ /:/g')
+declare -r JARS=$(
+  find "$ROOT" -name '*jar' |
+    xargs |
+    sed 's/jar /jar:/g'
+)
 
 export SCALA_HOME='/usr/share/scala-2.11'
 
